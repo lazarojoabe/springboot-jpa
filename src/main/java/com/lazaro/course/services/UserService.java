1233e66f1a87987fd0eb3a3fcb7e1113b69cd7dd
Lázaro Joabe <99762355+lazarojoabe@users.noreply.github.com>
2024-07-08 18:13:35 -0300

    service layer, component register

---
+++ src/main/java/com/lazaro/course/services/UserService.java
@@ -0,0 +1,25 @@
+package com.lazaro.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lazaro.course.entities.User;
+import com.lazaro.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id){
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
