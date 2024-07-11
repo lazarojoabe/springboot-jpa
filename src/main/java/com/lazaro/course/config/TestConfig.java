62e4d1c52902ce97ccbd0579b95b45a98a57311b
Lázaro Joabe <99762355+lazarojoabe@users.noreply.github.com>
2024-07-11 16:03:42 -0300

    Category created

--- src/main/java/com/lazaro/course/config/TestConfig.java
+++ src/main/java/com/lazaro/course/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.lazaro.course.entities.Category;
 import com.lazaro.course.entities.Order;
 import com.lazaro.course.entities.User;
 import com.lazaro.course.entities.enums.OrderStatus;
+import com.lazaro.course.repositories.CategoryRepository;
 import com.lazaro.course.repositories.OrderRepository;
 import com.lazaro.course.repositories.UserRepository;
 
@@ -21,8 +23,15 @@ public class TestConfig implements CommandLineRunner{
 	private UserRepository userRepository;
 	@Autowired
 	private OrderRepository orderRepository;
+	@Autowired
+	private CategoryRepository categoryRepository;
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 			
@@ -30,6 +39,7 @@ public class TestConfig implements CommandLineRunner{
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.DELIVERED, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.SHIPPED, u1); 
 		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 	}
