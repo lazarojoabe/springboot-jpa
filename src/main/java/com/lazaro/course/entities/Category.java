cedc2e2cf7d8ebd47a3c11c1eee849daaa3ca4ec
Lázaro Joabe <99762355+lazarojoabe@users.noreply.github.com>
2024-07-11 16:55:52 -0300

    many to many association join table

--- src/main/java/com/lazaro/course/entities/Category.java
+++ src/main/java/com/lazaro/course/entities/Category.java
@@ -5,10 +5,14 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 
@@ -21,7 +25,9 @@ public class Category implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
-	@Transient
+	
+		
+	@ManyToMany(mappedBy = "categories")
 	private Set<Product> products = new HashSet<>();
 	public Category() {}
 
@@ -52,6 +58,7 @@ public class Category implements Serializable{
 		return Objects.hash(id);
 	}
 
+	@JsonIgnore
 	public Set<Product> getProducts() {
 		return products;
 	}
