06c225dc8c3ca09d078fc5b09ef9ce812e03030a
Lázaro Joabe <99762355+lazarojoabe@users.noreply.github.com>
2024-07-12 18:34:30 -0300

    Payment created and associated to Order

--- src/main/java/com/lazaro/course/config/TestConfig.java
+++ src/main/java/com/lazaro/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.lazaro.course.entities.Category;
 import com.lazaro.course.entities.Order;
 import com.lazaro.course.entities.OrderItem;
+import com.lazaro.course.entities.Payment;
 import com.lazaro.course.entities.Product;
 import com.lazaro.course.entities.User;
 import com.lazaro.course.entities.enums.OrderStatus;
@@ -76,6 +77,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 	
--- src/main/java/com/lazaro/course/entities/Order.java
+++ src/main/java/com/lazaro/course/entities/Order.java
@@ -9,13 +9,16 @@ import java.util.Set;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.lazaro.course.entities.enums.OrderStatus;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -38,11 +41,12 @@ public class Order implements Serializable {
 	
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<OrderItem>();
-	
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL) //cascade mapps both entities to have the same id
+	private Payment payment;
 	public Order() {
 
 	}
-
+ 
 	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
@@ -76,6 +80,14 @@ public class Order implements Serializable {
 	}
 
 	
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
 	public Set<OrderItem> getItems() {
 		return items;
 	}
