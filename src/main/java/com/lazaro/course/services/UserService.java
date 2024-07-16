1931ac4326f1b98caaf8b4246b2ee85aea8770d3
Lázaro Joabe <99762355+lazarojoabe@users.noreply.github.com>
2024-07-16 18:19:12 -0300

    Exception handling - findById

--- src/main/java/com/lazaro/course/services/UserService.java
+++ src/main/java/com/lazaro/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.lazaro.course.entities.User;
 import com.lazaro.course.repositories.UserRepository;
+import com.lazaro.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -20,7 +21,7 @@ public class UserService {
 	
 	public User findById(Long id){
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User user) {
